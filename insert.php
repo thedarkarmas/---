12c8bdeac764c02a29f1e3ea9c20f4c31047a43b
thedarkarmas <76504699+thedarkarmas@users.noreply.github.com>
2021-02-10 10:45:45 +0700

    1.7

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
    require_once("dbcon.php");
   $sql = "INSERT INTO movie (movie_id,movie_name,movie_tday)
-VALUES ('{$_POST['movie_id']}','{$_POST['movie_name']}', '{$_POST['movie_tday']}'";
+VALUES ('{$_POST['movie_id']}','{$_POST['movie_name']}', '{$_POST['movie_tday']}')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
